a5d3910e64823521c5769108ce531c354cb8e41e
banafshe <banafshe.rahravi97@gmail.com>
2023-11-22 03:36:01 -0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h2>Atlas: The Ultimate Guide to the World</h2>
+    <p>Atlas is a website that lists every country with their flags and facts. You can explore the world map, search by
+      name or region, and filter by criteria. Learn about the world and its diversity with Atlas!</p>
+    <button>Region</button>
+    <button>Filter</button>
+  </div>
+  <section>
+    <ul>
+      <li>
+        <img>
+        <div>
+          <span>
+            <h3>Germany</h3>
+          </span>
+
+
+          <span></span>
+        </div>
+        <p>Federal Republic of Germany is an independent country in Western Europe with an estimated population of over 83
+          million people.</p>
+        <button>Learn more</button>
+      </li>
+
+
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  </section>
+</template>
+<script setup>
+</script>
+<style scoped></style>
